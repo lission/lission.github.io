eb8e7b826fb4355421c2a4534de05c6ed38ba85d
lisong <lisong@cestc.cn>
2024-06-12 21:25:15 +0800

    描述修改

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -48,7 +48,7 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `跑步时便该当心在跑步，跑步 ${yearLength} 年了，下面展示的是${yearStr}的数据`;
+  return `跑步时便当心在跑步，跑步 ${yearLength} 年了，下面展示的是${yearStr}的数据`;
 };
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
